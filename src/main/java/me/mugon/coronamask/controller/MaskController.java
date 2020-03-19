af78d7a1c9c015f831f03b036fbd83b2d48a81b6
mkshin96 <tlsanrhs96@gmail.com>
2020-03-19 14:20:02 +0900

    modified controller

--- src/main/java/me/mugon/coronamask/controller/MaskController.java
+++ src/main/java/me/mugon/coronamask/controller/MaskController.java
@@ -1,19 +1,16 @@
 package me.mugon.coronamask.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.mugon.coronamask.controller.dto.MaskResponseDto;
 import me.mugon.coronamask.domain.Store;
 import me.mugon.coronamask.service.MaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping(value = "/api")
 @Controller
 public class MaskController {
 
@@ -24,12 +21,17 @@ public class MaskController {
     예) '서울특별시 강남구' or '서울특별시 강남구 논현동'
     ('서울특별시' 와 같이 '시'단위만 입력하는 것은 불가능합니다.)
      */
-    @GetMapping("/masks")
+    @GetMapping("/")
+    public String welcome() {
+        return "index";
+    }
+
+    @GetMapping("/api/masks")
     public String getMasks(Model model, @RequestParam(value = "address", required = false) String address) {
         List<Store> masks = maskService.getMasks(address);
         if (masks != null) {
             model.addAttribute("masks", masks);
         }
-        return "/index";
+        return "index";
     }
 }
